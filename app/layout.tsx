83f11f42c7597caec5c1038f489fce02f2ab8cee
ronishdua <ronishdua@gmail.com>
2025-06-14 22:11:19 -0400

    tab logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,28 @@ import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Ronish Dua",
+  description: "Software engineer based in Washington, D.C.",
   icons: {
     icon: "/tabLogo.png",
     shortcut: "/tabLogo.png",
     apple: "/tabLogo.png",
+  },
+  openGraph: {
+    title: "Ronish Dua",
+    images: [
+      {
+        url: "/tabLogo.png",
+        width: 1200,
+        height: 630,
+        alt: "Ronish Dua Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Ronish Dua",
+    images: ["/tabLogo.png"],
   },
 };
 
